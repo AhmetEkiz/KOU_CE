386bc7e6a106a40d5923b3dfa98463dbcad7db06
AhmetEkiz <ekizoahmet9@gmail.com>
2022-11-10 15:42:13 +0300

    insertion_sort.c added

--- Data_Structures_and_Algorithms/w4_sorting_and_searching/insertion_sort.c
+++ Data_Structures_and_Algorithms/w4_sorting_and_searching/insertion_sort.c
@@ -4,7 +4,14 @@
 #include "../utils/utils.h"
 
 void insertion_sort(int list[], int size){
-	
+	int loc;  //location
+	for(int i=0; i < size-1; i++){
+		loc = i;
+		while(list[loc+1] < list[loc] && loc>-1){
+			exchange(&list[loc+1], &list[loc]);
+			loc--;
+		}
+	}
 }
 
 int main(){
@@ -12,8 +19,11 @@ int main(){
 	int list[] = {9, 7, 6, 15, 16, 5, 10, 11};
 	int size = sizeof(list)/sizeof(list[0]);
 
+	printf("Before sorting\n");
 	print_list(list, size);
-	// insertion_sort(list, size)
+	insertion_sort(list, size);
+	printf("\nAfter sorting\n");
+	print_list(list, size);
 
 	return 0;
 }
